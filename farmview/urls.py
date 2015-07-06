baaf57d4b221a89ec90a9570225b6a5461d590e7
Seongtaek Lim <EH71@Seongtaeks-MacBook-Air.local>
2015-07-06 01:12:56 -0700

    minor change: before AJAX

--- farmview/urls.py
+++ farmview/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-  url('^$', include('mapper.urls')),
+  url(r'^$', include('mapper.urls')),
   url(r'^admin/', include(admin.site.urls)),
 ]
--- mapper/views.py
+++ mapper/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import AdminSetting
 
 def index(request):
-  admin_setting = AdminSetting.objects.order_by('-pub_date')[0]
+  admin_settings = AdminSetting.objects.order_by('-pub_date')[0]
   context = {
-    'admin_setting': admin_setting
+    'admin_settings': admin_settings
   }
   return render(request, 'mapper/index.html', context)
