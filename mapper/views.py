28eb5cabf82ae1f3e6ec3066cc38b7593d5486cd
Seongtaek Lim <EH71@airbears2-10-142-138-117.AirBears2.1918.Berkeley.EDU>
2015-07-06 18:36:18 -0700

    added a missing file

--- mapper/views.py
+++ mapper/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from .models import AdminSetting
+from .models import SurveyQuestion
 
 def index(request):
   admin_settings = AdminSetting.objects.order_by('-pub_date')[0]
+  survey_questions = list(SurveyQuestion.objects.all())
+  query_fields = []
+  for survey_question in survey_questions:
+    type = survey_question.type
+    name = survey_question.name
+    field = {
+      'type': str(type),
+      'name': str(name)
+    }
+    query_fields.append(field)
   context = {
-    'admin_settings': admin_settings
+    'admin_settings': admin_settings,
+    'query_fields': query_fields
   }
   return render(request, 'mapper/index.html', context)
