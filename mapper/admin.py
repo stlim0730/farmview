aa3aadd907d0350ab4f1ef0e6a5ccfc3f9a9d40e
Seongtaek Lim <EH71@airbears2-10-142-139-150.AirBears2.1918.Berkeley.EDU>
2015-07-07 18:02:53 -0700

    admin action for query field selection

--- mapper/admin.py
+++ mapper/admin.py
@@ -13,8 +13,18 @@ class SurveyQuestionResource(resources.ModelResource):
   class Meta:
     model = SurveyQuestion
 
+def make_queryable(modeladmin, request, queryset):
+    queryset.update(queryable=True)
+make_queryable.short_description = 'Mark selected fields as queryable'
+
+def make_not_queryable(modeladmin, request, queryset):
+    queryset.update(queryable=False)
+make_not_queryable.short_description = 'Mark selected fields as NOT queryable'
+
+
 class SurveyQuestionAdmin(ImportExportModelAdmin):
   resource_class = SurveyQuestionResource
   list_display = ('id', 'name', 'type', 'queryable', 'label_english')
+  actions = [make_queryable, make_not_queryable]
 
 admin.site.register(SurveyQuestion, SurveyQuestionAdmin)
