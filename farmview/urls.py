71d95a41e10cb5317f7e085107a02f77550a353e
Bofan Chen <bofanchen1@gmail.com>
2015-12-06 13:58:39 -0800

    pages all viewable but mapbook & contact need to be fixed

--- farmview/urls.py
+++ farmview/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
   url(r'^admin/', include(admin.site.urls)),
   url(r'^map/$', include('map.urls')),
-  url(r'^$', include('pages.urls')),
+  url(r'^', include('pages.urls')),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.contrib.sitemaps import Sitemap
 
 def index(request):
   return render(request, 'pages/index.html')
+
+def about(request):
+  return render(request, 'pages/about.html')
+
+def mapbook(request):
+  return render(request, 'pages/mapbook.html')
+
+def contact(request):
+  return render(request, 'pages/contact.html')
