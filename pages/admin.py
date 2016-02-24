0a87eef958864710ef4c69df6378da7ee51bd687
Seongtaek Lim <EH71@Seongtaeks-MacBook-Air.local>
2016-02-23 17:16:21 -0800

    model and admin for mapbook

--- pages/admin.py
+++ pages/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Mapbook
+
+class MapbookAdmin(admin.ModelAdmin):
+  list_display = ('title', 'pub_date')
+
+admin.site.register(Mapbook, MapbookAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+class Mapbook(models.Model):
+  mapbook_id = models.AutoField(primary_key = True)
+  title = models.CharField(max_length = 80, unique = True)
+  slug = models.SlugField(max_length = 80, editable = False)
+  pub_date = models.DateTimeField(auto_now = True)
+  thumbnail_url = models.CharField(max_length = 50, unique = True)
+  text = models.TextField(blank = True)
+  enabled = models.BooleanField(default = True)
+  optional_note = models.CharField(max_length = 200, blank = True)
+  
+  def __unicode__(self):
+    return unicode(self.slug)
+
+  def save(self, *args, **kwargs):
+    self.slug = slugify(self.title)
+    super(Mapbook, self).save(*args, **kwargs)
