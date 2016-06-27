a6717fd57684feb638c4264c3329bd64be0e1050
Kimberly Bourque <kbourque@berkeley.edu>
2016-06-27 16:32:16 -0700

    added basic zinnia blog, can update through admin

--- farmview/urls.py
+++ farmview/urls.py
@@ -15,9 +15,34 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   url(r'^admin/', include(admin.site.urls)),
   url(r'^map/', include('map.urls')),
   url(r'^', include('pages.urls')),
-]
+  url(r'^weblog/', include('zinnia.urls')),
+  url(r'^comments/', include('django_comments.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+blog_urls = ([
+    url(r'^', include('zinnia.urls.capabilities')),
+    url(r'^search/', include('zinnia.urls.search')),
+    url(r'^sitemap/', include('zinnia.urls.sitemap')),
+    url(r'^trackback/', include('zinnia.urls.trackback')),
+    url(r'^blog/tags/', include('zinnia.urls.tags')),
+    url(r'^blog/feeds/', include('zinnia.urls.feeds')),
+    url(r'^blog/random/', include('zinnia.urls.random')),
+    url(r'^blog/authors/', include('zinnia.urls.authors')),
+    url(r'^blog/categories/', include('zinnia.urls.categories')),
+    url(r'^blog/comments/', include('zinnia.urls.comments')),
+    url(r'^blog/', include('zinnia.urls.entries')),
+    url(r'^blog/', include('zinnia.urls.archives')),
+    url(r'^blog/', include('zinnia.urls.shortlink')),
+    url(r'^blog/', include('zinnia.urls.quick_entry'))
+], 'zinnia')
+
+
+
+
