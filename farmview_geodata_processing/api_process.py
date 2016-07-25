ae645708e8728d71b46e969955770adb5041d9a0
Irene Lee <hyeonishl@gmail.com>
2016-07-24 18:45:06 -0700

    linking functions to admin in process

--- farmview_geodata_processing/api_process.py
+++ farmview_geodata_processing/api_process.py
@@ -27,16 +27,16 @@ def upload_dropbox(*args):
 # create new data set and save the returned id (=import_id needed for force_sync)
 # curl -v -H "Content-Type: application/json" -d '{"url":"https://dl.dropboxusercontent.com/s/6ngyvp8hy78qh4z/new_data_point.geojson?dl=0", "interval":"36000"}' "https://calo1.cartodb.com/api/v1/synchronizations/?api_key=API_KEY"
 # response = {"data_import":{"endpoint":"/api/v1/imports","item_queue_id":"120fd905-df10-49ba-99c6-20b2f515f169"},"id":"f874b4d4-4081-11e6-8c2a-0ecfd53eb7d3","name":null,"interval":36000,"url":"https://dl.dropboxusercontent.com/s/6ngyvp8hy78qh4z/new_data_point.geojson?dl=0","state":"created","user_id":"476e377b-b926-4595-9c71-1a73574ef706","created_at":"2016-07-02T18:22:44+00:00","updated_at":"2016-07-02T18:22:44+00:00","run_at":"2016-07-03T04:22:44+00:00","ran_at":"2016-07-02T18:22:44+00:00","modified_at":null,"etag":null,"checksum":"","log_id":null,"error_code":null,"error_message":null,"retried_times":0,"service_name":null,"service_item_id":null,"type_guessing":true,"quoted_fields_guessing":true,"content_guessing":false,"visualization_id":null,"from_external_source":false}
-# Takes in
-def create_sync_table(ona_id):
-    download_ona_data(ona_id)
+# Takes in FormData model.
+def create_sync_table(form_data):
+    download_ona_data(form_data.ona_id)
     # Create shared Dropbox URL
     db_url = upload_dropbox('new_data_point.geojson','new_data_polygon.geojson')
     url = 'https://calo1.cartodb.com/api/v1/synchronizations/?api_key=' + os.environ.get('CARTODB_API_KEY')
     header = {'Content-Type':'application/json'}
     res = requests.post(headers = header, data = {'url': file_url, 'interval':'36000'})
-    form = FormData(import_id = res['id'], dropbox_url = db_url)
-    form.save()
+    form_data.import_id, form_data.dropbox_url = res['id'], db_url
+    form_data.save()
 
 # calls sync_now. should have at least 15 min interval
 # curl -v -X "PUT" "https://calo1.cartodb.com/api/v1/synchronizations/import_id/sync_now?api_key=API_KEY" -H "Content-Length:0"
@@ -45,15 +45,16 @@ def force_sync(import_id):
     header = {'Content-Length':0}
     requests.put(url, headers=header)
 
-# using survey data 63362 and change the formats to geoJSON
+# using survey data id (curr=63362) and change the formats to geoJSON
 def download_ona_data(data_id):
     url = 'https://api.ona.io/api/v1/data/'+data_id
     header = {'Authorization': 'Token ' + os.environ.get('ONA_AUTH_TOKEN')}
     run_geoprocess((requests.get(url, headers=header)).json())
 
-form = FormData.objects.filter(ona_id = 63362)
+# form = FormData.objects.filter(ona_id = 63362)
 # if form data is already present with the given Ona survey data, force sync. If not, create a new sync table
-if form:
-    force_sync(form.import_id)
-else:
-    create_sync_table(63362)
+# if button clicked:
+#     force_sync(form.import_id)
+# else:
+#     create_sync_table(63362)
+# not sure how to retrieve public url after sharing
--- map/admin.py
+++ map/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Config
 from .models import Datafield
+from .models import FormData
+import sys
+sys.path.insert(0, '/farmview/farmview_geodata_processing')
 
 class ConfigAdmin(admin.ModelAdmin):
   list_display = ('pub_date', 'vizjson_url', 'optional_note')
@@ -11,3 +15,10 @@ class DatafieldAdmin(admin.ModelAdmin):
   list_display = ('datafield_id', 'datafield_name', 'data_sources', 'datafield_type', 'use_for_query_ui', 'use_for_detail_popup', 'enabled')
 
 admin.site.register(Datafield, DatafieldAdmin)
+
+class FormDataAdmin(admin.ModelAdmin):
+  list_display = ('formdata_id', 'ona_id', 'dropbox_url', 'last_synced_date', 'optional_note', 'button')
+  def button(self, obj):
+    return mark_safe('<input type="button" value="Force Sync">')
+
+admin.site.register(FormData, FormDataAdmin)
