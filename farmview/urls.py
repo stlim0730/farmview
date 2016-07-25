6856a31f71205f11cc09c462e817209a7e532647
Kimberly Bourque <kbourque@berkeley.edu>
2016-07-25 16:33:34 -0700

    Fixed up some CSS in the blog.

--- farmview/urls.py
+++ farmview/urls.py
@@ -26,22 +26,22 @@ urlpatterns = [
   url(r'^comments/', include('django_comments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-blog_urls = ([
-    url(r'^', include('zinnia.urls.capabilities')),
-    url(r'^search/', include('zinnia.urls.search')),
-    url(r'^sitemap/', include('zinnia.urls.sitemap')),
-    url(r'^trackback/', include('zinnia.urls.trackback')),
-    url(r'^blog/tags/', include('zinnia.urls.tags')),
-    url(r'^blog/feeds/', include('zinnia.urls.feeds')),
-    url(r'^blog/random/', include('zinnia.urls.random')),
-    url(r'^blog/authors/', include('zinnia.urls.authors')),
-    url(r'^blog/categories/', include('zinnia.urls.categories')),
-    url(r'^blog/comments/', include('zinnia.urls.comments')),
-    url(r'^blog/', include('zinnia.urls.entries')),
-    url(r'^blog/', include('zinnia.urls.archives')),
-    url(r'^blog/', include('zinnia.urls.shortlink')),
-    url(r'^blog/', include('zinnia.urls.quick_entry'))
-], 'zinnia')
+# blog_urls = ([
+#     url(r'^', include('zinnia.urls.capabilities')),
+#     url(r'^search/', include('zinnia.urls.search')),
+#     url(r'^sitemap/', include('zinnia.urls.sitemap')),
+#     url(r'^trackback/', include('zinnia.urls.trackback')),
+#     url(r'^blog/tags/', include('zinnia.urls.tags')),
+#     url(r'^blog/feeds/', include('zinnia.urls.feeds')),
+#     url(r'^blog/random/', include('zinnia.urls.random')),
+#     url(r'^blog/authors/', include('zinnia.urls.authors')),
+#     url(r'^blog/categories/', include('zinnia.urls.categories')),
+#     url(r'^blog/comments/', include('zinnia.urls.comments')),
+#     url(r'^blog/', include('zinnia.urls.entries')),
+#     url(r'^blog/', include('zinnia.urls.archives')),
+#     url(r'^blog/', include('zinnia.urls.shortlink')),
+#     url(r'^blog/', include('zinnia.urls.quick_entry'))
+# ], 'zinnia')
 
 
 
